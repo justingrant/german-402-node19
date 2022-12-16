029246f1aa352837abeee69908902357eaab0cb8
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 23:36:19 -0800

    remove calendar

--- test.mjs
+++ test.mjs
@@ -1,10 +1,8 @@
 import { strict as assert } from 'assert';
 const { equal } = assert;
 
-// import { Intl } from '@js-temporal/polyfill';
-
 // May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
-const options = { year: 'numeric', month: 'numeric', calendar: 'gregory' };
+const options = { year: 'numeric', month: 'numeric' };
 const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', options);
 const germanMonthYear = germanMonthYearFormatter.format(new Date (1976, 10, 15));
 equal (germanMonthYear, '11.1976');
