64f057b86dea3e78f611cf6ce3cabd31360a9234
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 20:43:02 -0800

    Add Temporal polyfill of Intl

--- test.mjs
+++ test.mjs
@@ -1,6 +1,8 @@
 import { strict as assert } from 'assert';
 const { equal } = assert;
 
+import { Intl } from '@js-temporal/polyfill';
+
 // May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
 const germanDateFormatter = new Intl.DateTimeFormat("de-AT", { dateStyle: "short", timeZone: 'UTC' });
 const germanDate = germanDateFormatter.format(0);
