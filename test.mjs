e47beaab7397ce402877ef23529e02f1d70dd50d
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 21:31:52 -0800

    try adding gregory calendar

--- test.mjs
+++ test.mjs
@@ -6,10 +6,10 @@ import { Intl } from '@js-temporal/polyfill';
 const epoch = new Date (1970,0,1)
 
 // May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
-const germanDateFormatter = new Intl.DateTimeFormat("de-AT", { dateStyle: "short" });
+const germanDateFormatter = new Intl.DateTimeFormat("de-AT", { dateStyle: 'short', calendar: 'gregory' });
 const germanDate = germanDateFormatter.format(epoch);
 equal (germanDate, '01.01.70');
 
-const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', { year: '2-digit', month: '2-digit' });
+const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', { year: '2-digit', month: '2-digit', calendar: 'gregory' });
 const germanMonthYear = germanMonthYearFormatter.format(epoch);
 equal (germanMonthYear, '01.70');
