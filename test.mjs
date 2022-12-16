6eaa8cfc5d4ba6c909be7b96b8b0ea0a733fc7a5
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 23:27:46 -0800

    refactor

--- test.mjs
+++ test.mjs
@@ -5,12 +5,11 @@ import { Intl } from '@js-temporal/polyfill';
 
 const epoch = new Date (1970,0,1)
 
-// May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
-const germanDateFormatter = new Intl.DateTimeFormat("de-AT", { dateStyle: 'short', calendar: 'gregory' });
-const germanDate = germanDateFormatter.format(epoch);
-equal (germanDate, '01.01.70');
+const optionsWithoutTimeZone = { year: 'numeric', month: 'numeric', calendar: 'gregory' };
+const optionsWithTimeZone = { year: 'numeric', month: 'numeric', timeZone: "Europe/Vienna", calendar: 'gregory' };
 
-const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', { year: 'numeric', month: 'numeric', timeZone: "Europe/Vienna", calendar: 'gregory' });
+// May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
+const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', optionsWithoutTimeZone);
 const germanMonthYear = germanMonthYearFormatter.format(new Date (1976, 10, 15));
 equal (germanMonthYear, '11.1976');
 
