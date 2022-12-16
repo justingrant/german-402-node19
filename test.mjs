9954125ba6b982e885ff5d58713ac2f8632af7b3
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 23:46:33 -0800

    Add version info

--- test.mjs
+++ test.mjs
@@ -1,4 +1,5 @@
 import { strict as assert } from 'assert';
+console.log(process.versions);
 const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', { year: 'numeric', month: 'numeric' });
 // This assertion passes in Node 18, fails in Node 19.2.0. But only in GH actions. Works OK in my local Node 19.2.0.
 assert.equal (germanMonthYearFormatter.format(new Date (1976, 10, 15)), '11.1976');
