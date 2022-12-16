091ab66f670a4999b721af4228509e490557ef87
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 23:29:35 -0800

    remove temporal

--- test.mjs
+++ test.mjs
@@ -1,15 +1,11 @@
 import { strict as assert } from 'assert';
 const { equal } = assert;
 
-import { Intl } from '@js-temporal/polyfill';
-
-const epoch = new Date (1970,0,1)
-
-const optionsWithoutTimeZone = { year: 'numeric', month: 'numeric', calendar: 'gregory' };
-const optionsWithTimeZone = { year: 'numeric', month: 'numeric', timeZone: "Europe/Vienna", calendar: 'gregory' };
+// import { Intl } from '@js-temporal/polyfill';
 
 // May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
-const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', optionsWithoutTimeZone);
+const options = { year: 'numeric', month: 'numeric', calendar: 'gregory' };
+const germanMonthYearFormatter = new Intl.DateTimeFormat('de-AT', options);
 const germanMonthYear = germanMonthYearFormatter.format(new Date (1976, 10, 15));
 equal (germanMonthYear, '11.1976');
 
