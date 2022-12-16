21a483d801b33a2fda3da33f6f7e8efb2f3fa199
Justin Grant <justingrant@users.noreply.github.com>
2022-12-15 20:51:43 -0800

    Try different DTF options

--- test.mjs
+++ test.mjs
@@ -4,6 +4,6 @@ const { equal } = assert;
 import { Intl } from '@js-temporal/polyfill';
 
 // May be an Intl bug: the de-AT date separator changed from "." to "/" in Node 19
-const germanDateFormatter = new Intl.DateTimeFormat("de-AT", { dateStyle: "short", timeZone: 'UTC' });
-const germanDate = germanDateFormatter.format(0);
+const germanDateFormatter = new Intl.DateTimeFormat("de-AT", { dateStyle: "short" });
+const germanDate = germanDateFormatter.format(new Date (1970,0,1));
 equal (germanDate, '01.01.70');
